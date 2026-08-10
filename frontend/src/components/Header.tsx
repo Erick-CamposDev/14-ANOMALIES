1f42d630a61f2099ae5ed502510ead446b1ded8e
Erick Fellipe <efrc22br@gmail.com>
2026-08-10 16:53:06 -0300

    Desenvolvendo a primeira interface inicial

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,3 +1,13 @@
+import "../css/Header.css";
+
 export default function Header() {
-  return <></>;
+  return (
+    <header>
+      <img
+        className="logo"
+        src="src/assets/14-Anomalies-LOGO.png"
+        alt="Logo oficial da 14 ANOMALIES"
+      />
+    </header>
+  );
 }
--- frontend/src/pages/InitialScreen.tsx
+++ frontend/src/pages/InitialScreen.tsx
@@ -1,7 +1,13 @@
+import "../css/InitialScreen.css";
+
 export default function InitialScreen() {
   return (
     <div className="initial-container">
-      <h1>14 ANOMALIES</h1>
+      <img
+        className="eye"
+        src="src/assets/Anomaly-eye.png"
+        alt="Imagem do Olho Anômalo"
+      />
       <div className="btns-container"></div>
     </div>
   );
