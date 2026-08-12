a436943c795d5b3b7f952d1686578fe836d34665
Erick Fellipe <efrc22br@gmail.com>
2026-08-12 16:11:41 -0300

    Teste do botão na página inicial

--- frontend/src/pages/InitialScreen.tsx
+++ frontend/src/pages/InitialScreen.tsx
@@ -1,3 +1,4 @@
+import Button from "../components/Button";
 import "../css/InitialScreen.css";
 
 export default function InitialScreen() {
@@ -8,7 +9,11 @@ export default function InitialScreen() {
         src="src/assets/Anomaly-eye.png"
         alt="Imagem do Olho Anômalo"
       />
-      <div className="btns-container"></div>
+      <h1>Olá, desafiante.</h1>
+      <div className="btns-container">
+        <Button variant="double" text="desafiar" icon="eye-fill" />
+        <Button variant="double" text="Sobre o jogo" icon="book-fill" />
+      </div>
     </div>
   );
 }
