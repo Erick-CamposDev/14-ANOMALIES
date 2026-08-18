8cf3c1bd2e6569d070aafe452e8c17ced0a489e0
Erick Fellipe <efrc22br@gmail.com>
2026-08-18 15:05:40 -0300

    Adicionado arquivos de constantes

---
+++ frontend/src/constants/stringsArray.ts
@@ -0,0 +1,28 @@
+export const eyeImages: string[] = [
+  "src/assets/Anomaly-Eye.png",
+  "src/assets/Anomaly-Eye-Closed.png",
+];
+
+export const eyeMessages: string[] = [
+  "Olá...",
+  "Seja bem vindo desafiante.",
+  "Eu sou o olho Anômalo.",
+  "Então...",
+  "Vamos começar?",
+  "Vai ser instigante.",
+];
+
+export const introMessages: string[] = [
+  "Aguardando conexão com o ambiente",
+  "Conexão estabelecida...",
+  "Aguardando autorização de Obcaozwo 14",
+  "Autorização concedida...",
+  "Lendo dados do vídeo",
+  "ERRO CRÍTICO",
+  "A leitura foi comprometida",
+  "14 ANOMALIAS foram detectadas",
+  "Será necessário que elas sejam passadas.",
+  "É com você desafiante.",
+  "Boa sorte.",
+  "Obcaozwo 14",
+];
