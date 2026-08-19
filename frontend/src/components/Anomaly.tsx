c05bcf63581d964ba6e32a276f71c65c5c823108
Erick Fellipe <efrc22br@gmail.com>
2026-08-19 16:37:53 -0300

    Criado componente de enigma

---
+++ frontend/src/components/Anomaly.tsx
@@ -0,0 +1,12 @@
+import { useEffect } from "react";
+
+export default function Anomaly() {
+  useEffect(() => {}, []);
+
+  return (
+    <div className="anomaly-container">
+      <div className="anomaly-content"></div>
+      <div className="input-container"></div>
+    </div>
+  );
+}
