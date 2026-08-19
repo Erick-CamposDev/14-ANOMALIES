84cf3ff0f6aaa84f10800351e954082cd15c660f
Erick Fellipe <efrc22br@gmail.com>
2026-08-19 16:37:23 -0300

    Concluido logo não aparecer nos enigmas

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,13 +1,23 @@
+import { useLocation, useParams } from "react-router-dom";
 import "../css/Header.css";
 
 export default function Header() {
+  const { pathname } = useLocation();
+  const { number } = useParams();
+
+  const home = pathname === "/";
+  const riddle = pathname.startsWith("/anomaly");
+
   return (
     <header>
-      <img
-        className="logo"
-        src="src/assets/14-Anomalies-logo.png"
-        alt="Logo oficial da 14 ANOMALIES"
-      />
+      {home && (
+        <img
+          className="logo"
+          src="src/assets/14-Anomalies-logo.png"
+          alt="Logo oficial da 14 ANOMALIES"
+        />
+      )}
+      {riddle && <h1 className="anomaly-title">ANOMALIA {number}</h1>}
     </header>
   );
 }
