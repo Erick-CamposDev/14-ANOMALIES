6005e43538541f96909bd51dadf33cc557b08e64
Erick Fellipe <efrc22br@gmail.com>
2026-08-20 16:28:28 -0300

    Adicionado parametro id na rota de criar progresso

--- backend/src/routes/progress-routes.ts
+++ backend/src/routes/progress-routes.ts
@@ -8,7 +8,7 @@ import {
 
 export const progressRoute = Router();
 
-progressRoute.post("/start/", startProgressGame);
+progressRoute.post("/start/:id", startProgressGame);
 progressRoute.get("/progress/:id", getPlayerProgress);
 progressRoute.patch("/progress/:id", updatePlayerProgress);
 progressRoute.post("/reset/:playerId", resetProgress);
