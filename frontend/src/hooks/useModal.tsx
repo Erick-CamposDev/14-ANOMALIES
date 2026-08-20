dbbd2193fe883df6f996eb911a260825dfebdcf0
Erick Fellipe <efrc22br@gmail.com>
2026-08-20 15:50:25 -0300

    Adicionado hook para modals

---
+++ frontend/src/hooks/useModal.tsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+export type ModalType = "error" | "about" | "hint" | null;
+
+export function useModal() {
+  const [modal, setModal] = useState<ModalType>(null);
+  const onOpen = (type: ModalType) => setModal(type);
+  const onClose = () => setModal(null);
+
+  return { modal, onOpen, onClose };
+}
