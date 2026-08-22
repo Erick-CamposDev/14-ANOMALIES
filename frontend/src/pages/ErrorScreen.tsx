d9652cb17ed9b5921433cd2128a64464a27510ba
Erick Fellipe <efrc22br@gmail.com>
2026-08-22 16:17:02 -0300

    Desenvolvimento da página de erro quase concluída

--- frontend/src/pages/ErrorScreen.tsx
+++ frontend/src/pages/ErrorScreen.tsx
@@ -1,7 +1,23 @@
+import { useLocation } from "react-router-dom";
+import CrimsonEye from "../assets/Crimson Eye.png";
+import "../css/ErrorScreen.css";
+
+interface ErrorState {
+  message: string;
+}
+
 export default function ErrorScreen() {
+  const location = useLocation();
+  const state = location.state as ErrorState;
+
   return (
-    <>
-      <h2>ERROR</h2>
-    </>
+    <div className="error-container">
+      <img
+        className="crimson-eye"
+        src={CrimsonEye}
+        alt="Imagem do olho anômalo em chamas"
+      />
+      <h2>{state.message}</h2>
+    </div>
   );
 }
