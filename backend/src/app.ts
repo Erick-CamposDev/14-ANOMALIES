56a5031ddab9b79ccfb35c023091ac961b93e4ee
Erick Fellipe <efrc22br@gmail.com>
2026-08-24 17:28:58 -0300

    Adicionado middleware para controlar o caminho de imagem do front para o back

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,5 @@
 import express, { json } from "express";
-
+import path from "path";
 import cors from "cors";
 import { riddleRoute } from "./routes/riddle-routes";
 import { progressRoute } from "./routes/progress-routes";
@@ -9,6 +9,10 @@ export default function createApp() {
 
   app.use(json());
   app.use(cors());
+  app.use(
+    "/assets",
+    express.static(path.join(__dirname, "../../frontend/src/assets")),
+  );
   app.use("/14anomalies/", riddleRoute);
   app.use("/14anomalies/", progressRoute);
 
