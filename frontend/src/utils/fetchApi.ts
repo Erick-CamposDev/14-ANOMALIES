8e610b579e2a1676e6bef5b134011ff666aab618
Erick Fellipe <efrc22br@gmail.com>
2026-08-24 17:25:00 -0300

    Ajustes na função de fetchApi

--- frontend/src/utils/fetchApi.ts
+++ frontend/src/utils/fetchApi.ts
@@ -2,14 +2,14 @@ type HttpMethods = "GET" | "POST" | "PATCH" | "PUT" | "DELETE";
 
 export async function fetchAPI(
   route: string,
-  method?: HttpMethods,
+  method: HttpMethods = "GET",
   body?: unknown,
 ) {
   const apiURL = import.meta.env.VITE_API_URL;
 
   try {
     const response = await fetch(`${apiURL}/${route}`, {
-      method: method ?? "GET",
+      method: method,
       headers: {
         "Content-Type": "application/json",
       },
@@ -18,16 +18,15 @@ export async function fetchAPI(
 
     const data = await response.json();
 
-    if (!response.ok) {
-      console.log(`STATUS: ${response.status}`);
-      console.log("BODY:", data);
-      throw new Error(`Erro ao buscar enigma`);
-    }
-
-    return { riddleData: data, status: response.status };
+    return { riddleData: data, status: response.status, ok: response.ok };
   } catch (error) {
-    if (error instanceof Error) {
-      console.log(error.message);
-    }
+    console.log(error);
   }
+  return {
+    riddleData: {
+      message: "O servidor não foi inicializado",
+    },
+    status: 500,
+    ok: false,
+  };
 }
