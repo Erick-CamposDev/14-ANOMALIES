42f955cfcc7e49088500f7c01a6310f66728ab4f
Erick Fellipe <efrc22br@gmail.com>
2026-08-25 16:20:26 -0300

    Removido middleware de conexão de arquivos

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,4 @@
 import express, { json } from "express";
-import path from "path";
 import cors from "cors";
 import { riddleRoute } from "./routes/riddle-routes";
 import { progressRoute } from "./routes/progress-routes";
@@ -9,10 +8,6 @@ export default function createApp() {
 
   app.use(json());
   app.use(cors());
-  app.use(
-    "/assets",
-    express.static(path.join(__dirname, "../../frontend/src/assets")),
-  );
   app.use("/14anomalies/", riddleRoute);
   app.use("/14anomalies/", progressRoute);
 
