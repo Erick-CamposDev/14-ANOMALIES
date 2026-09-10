3ea96d7b4f3db758925f151700bb03b42f245179
Erick Fellipe <efrc22br@gmail.com>
2026-09-10 14:45:50 -0300

    corrigido bug de tipagem e de falha de build

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,8 @@
 import createApp from "./app";
+import "dotenv/config";
 
 const app = createApp();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando em: http://localhost:${PORT}`);
